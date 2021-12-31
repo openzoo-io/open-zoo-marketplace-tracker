052e6fa6b7a1c26c7c4b8cdc107cb6e34142b25e
lolieatapple <lolieatapple@gmail.com>
2021-12-31 22:49:41 +0800

    update catch and restart

--- app.js
+++ app.js
@@ -31,4 +31,15 @@ const connect = () => {
   })
 }
 
+process.on('unhandledRejection', (err) => {
+  console.error(err);
+  process.exit(1);
+});
+
+process.on('uncaughtException', (err) => {
+  console.error(err);
+  process.exit(1);
+});
+
+
 connect();
